eaa8a2810f1fb1ea8098a617f00dbe512801c26f
Hanna.Likhuta <Hanna.Likhuta@itechart-group.com>
2020-11-18 22:53:15 +0300

    add sidedrawer, create portal

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -2,23 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
+import SideDrawer from './SideDrawer';
+import NavLinks from './NavLinks';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">YourPlaces</Link>
-      </h1>
-      <nav>
-        ...
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+
+    </React.Fragment>
   );
 };
 
