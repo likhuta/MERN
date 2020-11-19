5a3609408c5a4fc19aa35adcdbe14076539710f0
Hanna <hanna@wsa-121-71b.icx.local>
2020-11-19 15:58:57 +0300

    add animation

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import SideDrawer from './SideDrawer';
 import NavLinks from './NavLinks';
+import Backdrop from '../UIElements/Backdrop';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
+
   return (
     <React.Fragment>
-
-      <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} /> }
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav" >
           <NavLinks />
         </nav>
       </SideDrawer>
+
+
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
           <span />
           <span />
           <span />
