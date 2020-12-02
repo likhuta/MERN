78a6c4f7fd2acac26428a15255867ae652b7a44a
Hanna.Likhuta <Hanna.Likhuta@itechart-group.com>
2020-12-02 19:03:14 +0300

    add populate for get all places for userid

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -57,19 +57,22 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
+  // let places;
+  let userWithPlaces;
   try {
-    const places = await Place.find({creator: userId});
+    // places = await Place.find({creator: userId});
+    userWithPlaces = await User.findById(userId).populate('places');
 
   } catch (err) {
     const error = new HttpError('Fetching places failed, please try later', 500);
     return next(error);
   }
 
-  if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(new HttpError("Could not find a places for provided uid", 404));
   }
 
-  res.status(200).json({ places: places.map(place => place.toObject({getters: true})) });
+  res.status(200).json({ places: userWithPlaces.places.map(place => place.toObject({getters: true})) });
 };
 
 const createPlace = async (req, res, next) => {
