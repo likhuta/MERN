62f61098bc004c127e48d1e86711a56742a52a97
Hanna.Likhuta <Hanna.Likhuta@itechart-group.com>
2020-12-08 00:03:28 +0300

    update place

--- frontend/src/places/components/PlaceList.js
+++ frontend/src/places/components/PlaceList.js
@@ -22,10 +22,10 @@ const PlaceList = (props) => {
         <PlaceItem
           key={place.id}
           id={place.id}
-          image={place.imageUrl}
+          image={place.image}
           title={place.title}
           description={place.description}
-          address={place.description}
+          address={place.address}
           creatorId={place.creatoe}
           coordinates={place.location}
         />
